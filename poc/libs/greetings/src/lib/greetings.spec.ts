a31195b599e1c8072f115541f07b4ae5a2cea4c2
Renan Modenese <renan.modenese@gmail.com>
2023-08-18 09:29:26 -0300

    test: corrige nome do teste

--- poc/libs/greetings/src/lib/greetings.spec.ts
+++ poc/libs/greetings/src/lib/greetings.spec.ts
@@ -1,7 +1,7 @@
 import { greetings } from './greetings';
 
-describe('translate', () => {
-  test('Deve retornar a string "hello world"', () => {
+describe('greetings', () => {
+  test('deve retornar a string "hello world"', () => {
     expect(greetings()).toEqual('hello world');
   })
 })
